4ed69a212d1859f465d73c661d1783d78532d306
Princy159 <princyheryzo@gmail.com>
2024-03-28 14:56:33 +0300

    dashboard

--- src/main/java/mg/fertilize/tp_qualimetrie/TpQualimetrieApplication.java
+++ src/main/java/mg/fertilize/tp_qualimetrie/TpQualimetrieApplication.java
@@ -2,8 +2,10 @@ package mg.fertilize.tp_qualimetrie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"mg.fertilize.tp_qualimetrie.entity"})
 public class TpQualimetrieApplication {
 
 	public static void main(String[] args) {
--- src/main/java/mg/fertilize/tp_qualimetrie/controler/TacheController.java
+++ src/main/java/mg/fertilize/tp_qualimetrie/controler/TacheController.java
@@ -1,4 +1,5 @@
 package mg.fertilize.tp_qualimetrie.controler;
 
 public class TacheController {
+
 }
